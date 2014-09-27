927572cf47e741e41ab48a39f447198986f4f3ba
Helder <he7d3r@gmail.com>
2014-09-27 20:48:58 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/createDiffLinks.js
+++ src/createDiffLinks.js
@@ -4,26 +4,26 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function createDiffLinks(){
-	$('#mw-content-text').find('pre').each(function(){
-		var i,
-			reDiffNumber = /#(\d+).*$/g,
-			diffLink = '<a href="' + mw.config.get( 'wgServer' ) +
-				mw.config.get( 'wgScript' ) + '?diff=' + '$1">#$1</a>',
-			$this = $(this),
-			lines = $this.html().split( '\n' );
-		for( i = 0; i< lines.length; i += 1 ){
-			// \bexample.com   #123456
-			lines[i] = lines[i].replace( reDiffNumber, diffLink );
-		}
-		$this.html( lines.join( '\n' ) );
-	});
-}
+	function createDiffLinks() {
+		$('#mw-content-text').find('pre').each(function () {
+			var i,
+				reDiffNumber = /#(\d+).*$/g,
+				diffLink = '<a href="' + mw.config.get( 'wgServer' ) +
+					mw.config.get( 'wgScript' ) + '?diff=' + '$1">#$1</a>',
+				$this = $(this),
+				lines = $this.html().split( '\n' );
+			for ( i = 0; i < lines.length; i += 1 ) {
+				// \bexample.com   #123456
+				lines[i] = lines[i].replace( reDiffNumber, diffLink );
+			}
+			$this.html( lines.join( '\n' ) );
+		});
+	}
 
-if ( mw.config.get( 'wgPageName' ) === 'MediaWiki:Spam-blacklist' && mw.config.get( 'wgAction' ) === 'view' ) {
-	$( createDiffLinks );
-}
+	if ( mw.config.get( 'wgPageName' ) === 'MediaWiki:Spam-blacklist' && mw.config.get( 'wgAction' ) === 'view' ) {
+		$( createDiffLinks );
+	}
 
 }( mediaWiki, jQuery ) );
